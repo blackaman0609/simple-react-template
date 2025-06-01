5f16851d80526f0262143883277386b5af757511
Aman Kumar <amankumar9142424868@gmail.com>
2025-06-01 17:52:46 +0000

    react_simple_template

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+// import './style.css';
+import Header from './components/Header';
+import Footer from './components/Footer';
+
+export default function App() {
+  return (
+  <div>
+  <Header />
+  <h1>Hello World</h1>
+  <p>Let us learn ReactJS</p>
+  <Footer />
+  </div>
+  );
+}
